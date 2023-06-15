369ce991b3af83a919d8732cfb2a01aa9686bf41
HusseinJX <90575835+HusseinJX@users.noreply.github.com>
2023-06-16 02:10:01 +0300

    navlogout

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import Login from "./components/system/Login";
+import Navbar from "./components/system/Navbar";
 
 const App = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
   const [isLoadingToken, setIsLoadingToken] = useState(true);
 
   useEffect(() => {
-    const storedToken = Cookies.get('token');
+    const storedToken = Cookies.get("token");
     if (storedToken) {
       setToken(storedToken);
     }
     setIsLoadingToken(false);
   }, []);
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': 'api_key34',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setToken(data.token);
-        Cookies.set('token', data.token);
-      } else {
-        throw new Error('Login failed');
-      }
-    } catch (error) {
-      console.error('Error logging in:', error);
-    }
-  };
-
   const handleLogout = () => {
-    setToken('');
-    Cookies.remove('token');
-  };
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+    setToken("");
+    Cookies.remove("token");
   };
 
   if (isLoadingToken) {
@@ -59,26 +28,11 @@ const App = () => {
     <div>
       {token ? (
         <div>
+          <Navbar handleLogout={handleLogout} />
           <h1>Welcome!</h1>
-          <button onClick={handleLogout}>Logout</button>
         </div>
       ) : (
-        <div>
-          <h1>Login</h1>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <button onClick={handleLogin}>Login</button>
-        </div>
+        <Login setToken={setToken} />
       )}
     </div>
   );
