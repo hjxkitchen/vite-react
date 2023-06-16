7042d724637f4fe5141cde4527770d297d78537f
HusseinJX <90575835+HusseinJX@users.noreply.github.com>
2023-06-16 17:04:30 +0300

    deploybuild2

--- src/components/system/Login.jsx
+++ src/components/system/Login.jsx
@@ -22,7 +22,7 @@ const Login = ({ setToken, token }) => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch(import.meta.env.VITE_API_URL + "/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
