4e9af72bbd2a176b3350bf16741c40978b33cf25
HusseinJX <90575835+HusseinJX@users.noreply.github.com>
2023-06-21 08:34:57 +0300

    2 servers

--- src/system/components/system/GoogleLogin.jsx
+++ src/system/components/system/GoogleLogin.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GoogleAuth = () => {
   const handleGoogleLogin = () => {
-    window.location.href = import.meta.env.VITE_API_URL + "/auth/google"; // Redirect to the server route for Google OAuth authentication
+    window.location.href = import.meta.env.VITE_G_API_URL + "/auth/google"; // Redirect to the server route for Google OAuth authentication
   };
 
   return <button onClick={handleGoogleLogin}>Sign in with Google</button>;
