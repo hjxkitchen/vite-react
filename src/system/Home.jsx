420111647177871447d30534cbba71c8cbdc163a
HusseinJX <90575835+HusseinJX@users.noreply.github.com>
2023-06-23 16:10:11 +0300

    extract list crud component

--- src/system/Home.jsx
+++ src/system/Home.jsx
@@ -8,6 +8,7 @@ import Footer from "../system/components/Footer";
 
 import AddModal from "./../system/components/crud/AddModal";
 import EditModal from "../system/components/crud/EditModal";
+import List from "../system/components/crud/List";
 
 import Upload from "../system/components/upload/Upload";
 import UploadMultiple from "../system/components/upload/UploadMultiple";
@@ -15,48 +16,6 @@ import UploadMultiple from "../system/components/upload/UploadMultiple";
 const Home = () => {
   const { t } = useTranslation();
   const { theme } = useContext(ThemeContext);
-
-  //   get products
-  const [products, setProducts] = useState([]);
-
-  //   get token from cookie
-  const token = Cookies.get(import.meta.env.VITE_COOKIE_NAME);
-
-  useEffect(() => {
-    // GET WITH AXIOS HEADERS
-    axios
-      .get(import.meta.env.VITE_API_URL + "/api/Product", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "x-api-key": import.meta.env.VITE_API_KEY,
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
-        setProducts(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  //   delete product
-  const handleDelete = (id) => {
-    axios
-      .delete(import.meta.env.VITE_API_URL + "/api/Product/" + id, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "x-api-key": import.meta.env.VITE_API_KEY,
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
-        window.location.reload();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
   return (
     <>
@@ -75,27 +34,7 @@ const Home = () => {
         <UploadMultiple />
         <AddModal />
         <div className="container">
-          {products.map((product) => (
-            <div className="card mt-4" key={product.id}>
-              <div className="card-body">
-                <h5 className="card-title">{product.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  {product.price}
-                </h6>
-                <p className="card-text">{product.description}</p>
-              </div>
-              <div className="card-footer">
-                {/* EDIT, DELETE BUTTON */}
-                <EditModal product={product} />
-                <button
-                  className={"btn btn-" + theme + " mt-4 mb-4"}
-                  onClick={() => handleDelete(product.id)}
-                >
-                  {t("delete")}
-                </button>
-              </div>
-            </div>
-          ))}
+          <List />
         </div>
         <br />
         <br />
